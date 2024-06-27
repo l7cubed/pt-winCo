a417827ec7a519f006875724dc2d2613ee4688e0
L7 <161780994+l7cubed@users.noreply.github.com>
2024-06-27 00:18:35 +0000

    vaults

--- apps/app/src/vaultLists/default/base.ts
+++ apps/app/src/vaultLists/default/base.ts
@@ -5,7 +5,7 @@ export const baseVaults: VaultList['tokens'] = [
   {
     chainId: NETWORK.base,
     address: '0x82536B14f6D89E8409B292df5e93C14056aeEdEE',
-    name: 'Win NOUNS',
+    name: 'WinNOUNS-Staking',
     decimals: 18,
     symbol: 'winNOUNS',
     logoURI: `https://i.imgur.com/InzJwXV.png`,
@@ -16,5 +16,35 @@ export const baseVaults: VaultList['tokens'] = [
         name: 'NounsToken'
       }
     }
-  }
+  },
+  {
+    chainId: NETWORK.base,
+    address: '0x0B0b13Aa1d92Cc8A944F4Ed03aE6c7F8A4b3bd12',
+    name: 'WinWETH-Aloe',
+    decimals: 18,
+    symbol: 'winWETH',
+    logoURI: `https://i.imgur.com/mz8MIwm.png`,
+    extensions: {
+      underlyingAsset: {
+        address: '0x4200000000000000000000000000000000000006',
+        symbol: 'WETH',
+        name: 'NounsToken'
+      }
+    }
+  },
+  {
+    chainId: NETWORK.base,
+    address: '0x88C57d18074b7460122f4Da27BBe3948b71b0B72',
+    name: 'WinNOUNS-Aloe',
+    decimals: 18,
+    symbol: 'winNOUNS',
+    logoURI: `https://i.imgur.com/InzJwXV.png`,
+    extensions: {
+      underlyingAsset: {
+        address: '0x0a93a7BE7e7e426fC046e204C44d6b03A302b631',
+        symbol: 'NOUNS',
+        name: 'NounsToken'
+      }
+    }
+  },
 ]
