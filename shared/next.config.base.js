e385557c557fbc8faa53dedc131ce740c825a966
L7 <161780994+l7cubed@users.noreply.github.com>
2024-07-01 01:17:39 +0000

    n

---
+++ shared/next.config.base.js
@@ -0,0 +1,31 @@
+module.exports = {
+    reactStrictMode: true,
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.fallback = { fs: false, net: false, tls: false };
+      }
+      return config;
+    },
+    i18n: {
+      locales: ['en', 'es', 'de', 'fr', 'hi', 'it', 'ko', 'pt', 'tr', 'zh', 'ru', 'fil', 'uk'],
+      defaultLocale: 'en'
+    },
+    async headers() {
+      return [
+        {
+          source: '/api/:path*',
+          headers: [
+            { key: 'Access-Control-Allow-Credentials', value: 'true' },
+            { key: 'Access-Control-Allow-Origin', value: '*' },
+            { key: 'Access-Control-Allow-Methods', value: 'GET' },
+            {
+              key: 'Access-Control-Allow-Headers',
+              value:
+                'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+            }
+          ]
+        }
+      ];
+    }
+  };
+  
